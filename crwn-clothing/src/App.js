da1fe78aa69353de123d86e18cb27da0cc1d4638
thienhd1998 <thienhd2510@gmail.com>
2020-03-06 00:27:52 +0700

    Sign in

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -3,28 +3,19 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component'
-
-const Hatspage = (props) => (
-  <div>
-    <button onClick={() => props.history.push('/topic')}>Click here</button>
-    <h1>HATS PAGE</h1>
-  </div>
-)
-
-const Topic = () => (
-  <div>
-    <h1>Topic</h1>
-  </div>
-)
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'; 
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={Hatspage} />
-        <Route exact path='/topic' component={Topic} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
