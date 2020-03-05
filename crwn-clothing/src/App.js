bdc0723ff7aee84db0bf805a088eaf17bcfb4dc4
thienhd1998 <thienhd2510@gmail.com>
2020-03-05 10:21:33 +0700

    Add vs code

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 
+const Hatspage = (props) => (
+  <div>
+    <button onClick={() => props.history.push('/topic')}>Click here</button>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+
+const Topic = () => (
+  <div>
+    <h1>Topic</h1>
+  </div>
+)
+
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop/hats' component={Hatspage} />
+        <Route exact path='/topic' component={Topic} />
+      </Switch>
     </div>
   );
 }
