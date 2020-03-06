b1ad42d6f70db5a729ba8b33b4d6389bd16b79bb
thienhd1998 <thienhd2510@gmail.com>
2020-03-06 10:55:46 +0700

    Add header component and set up firebase and login with google account

---
+++ crwn-clothing/src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyAN2vKMKRwBVhiUENb-PB_Xxl-L_Y3q0cI",
+  authDomain: "crwn-db-9e2fb.firebaseapp.com",
+  databaseURL: "https://crwn-db-9e2fb.firebaseio.com",
+  projectId: "crwn-db-9e2fb",
+  storageBucket: "crwn-db-9e2fb.appspot.com",
+  messagingSenderId: "198365125554",
+  appId: "1:198365125554:web:af6638ab4ff75fb835e2ab",
+  measurementId: "G-3L0W5C2141"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
